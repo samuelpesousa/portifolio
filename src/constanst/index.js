d3ceb7fd8e632f90d2156854e4eeff1c9496f93c
samuelpesousa <samuelpsousa@hotmail.com>
2025-11-10 15:34:26 -0300

    feat: Add new components for Hero section and Button

---
+++ src/constanst/index.js
@@ -0,0 +1,11 @@
+export const words  = [
+    { text: 'Ideais', imgPath: '/images/ideas.svg' },
+    { text: 'Conceitos', imgPath: '/images/concepts.svg' },
+    { text: 'Designs', imgPath: '/images/designs.svg' },
+    { text: 'Código', imgPath: '/images/code.svg' },
+    { text: 'Ideais', imgPath: '/images/ideas.svg' },
+    { text: 'Conceitos', imgPath: '/images/concepts.svg' },
+    { text: 'Designs', imgPath: '/images/designs.svg' },
+    { text: 'Código', imgPath: '/images/code.svg' },
+    
+];
