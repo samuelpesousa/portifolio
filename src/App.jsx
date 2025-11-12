d96a0ddef8b0392b251f72e2864371e76daaac9f
samuelpesousa <samuelpsousa@hotmail.com>
2025-11-12 08:21:10 -0300

    feat: Implement Navbar and LogoSection components, enhance ShowcaseSection with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
+import Navbar from './components/Navbar.jsx'
+import LogoSection from './components/LogoSection.jsx'
 
 const App = () => {
     return(
       <>
+      <Navbar />
         <Hero />
         <ShowcaseSection />
+        <LogoSection />
       </>
     )
 }
