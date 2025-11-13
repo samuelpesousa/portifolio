e84d1faf4ff8dd20713258db845429113c9dbe64
samuelpesousa <samuelpsousa@hotmail.com>
2025-11-13 15:25:16 -0300

    feat: Refactor App structure, add FeatureCards and ExperienceSection, and enhance Hero with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
 import Navbar from './components/Navbar.jsx'
-import LogoSection from './components/LogoSection.jsx'
+import LogoSection from './sections/LogoSection.jsx'
+import FeatureCards from './sections/FeatureCards.jsx'
+import ExperienceSection from './sections/ExperienceSection.jsx'
 
 const App = () => {
     return(
@@ -11,6 +13,8 @@ const App = () => {
         <Hero />
         <ShowcaseSection />
         <LogoSection />
+        <FeatureCards />
+        <ExperienceSection />
       </>
     )
 }
